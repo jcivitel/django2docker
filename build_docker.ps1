10bd7a6f00d480c440323e37d1df78b0d3f11221
Jan Civitelli <jan.civitelli@dolphin-it.de>
2023-01-03 12:29:10 +0100

    added timestamp in release tag

--- build_docker.ps1
+++ build_docker.ps1
@@ -3,5 +3,5 @@ git clone <git_repo> git_clone
 cd git_clone
 git submodule update --init --recursive -j 4
 cd ..
-docker build --no-cache -t docker-django:dev .
+docker build --no-cache -t docker-django:dev_$(Get-Date -format "yyMMdd") .
 Remove-Item -Force -Recurse git_clone/*; echo "" > git_clone/.gitkeep
