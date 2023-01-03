6426d53ca7a946e87569bceee6fefb3f7510e8eb
Jan Civitelli <jan.civitelli@dolphin-it.de>
2023-01-03 16:29:31 +0100

    add build_docker.sh for Linux execution

---
+++ build_docker.sh
@@ -0,0 +1,7 @@
+rm -rf git_clone/*
+git clone <git_repo> git_clone
+cd git_clone
+git submodule update --init --recursive -j 4
+cd ..
+docker build --no-cache -t docker-django:dev_$(Get-Date -format "yyMMdd") .
+rm -rf git_clone/*; echo "" > git_clone/.gitkeep
